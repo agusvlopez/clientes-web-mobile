c0a0a5e099f9714b10a64ef15b46cc2f53fde2f7
agusvlopez <agusvlopez@gmail.com>
2023-09-15 11:40:56 -0300

    add: Loader component

--- clase-03/vite-project/src/components/BaseButton.vue
+++ clase-03/vite-project/src/components/BaseButton.vue
@@ -1,15 +1,31 @@
 <script>
+import Loader from './Loader.vue';
+
     export default {
-        name: 'BaseButton'
-    }
+    name: 'BaseButton',
+    props: {
+        loading: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    components: { Loader }
+}
 
 </script>
 
 <template>
     <button 
     class="transition w-full p-1.5 rounded bg-orange-700 text-white hover:bg-orange-600 active:bg-orange-800
-    motion-reduce:transition-none"
+    motion-reduce:transition-none disabled:bg-orange-400"
     type="submit"
-    ><slot>Enviar</slot>
+    :disabled="loading"
+    >
+    <div v-if="!loading">
+        <slot>Enviar</slot>
+    </div>
+    <div v-else>
+        <Loader></Loader>
+    </div>
     </button>
 </template>
--- clase-03/vite-project/src/pages/Chat.vue
+++ clase-03/vite-project/src/pages/Chat.vue
@@ -7,13 +7,16 @@ import BaseButton from "../components/BaseButton.vue";
 import BaseLabel from "../components/BaseLabel.vue";
 import BaseInput from "../components/BaseInput.vue";
 import BaseTextarea from "../components/BaseTextarea.vue";
+import Loader from "../components/Loader.vue";
 
 export default {
     name: "Chat",
-    components: { BaseButton, BaseLabel, BaseInput, BaseTextarea },
+    components: { BaseButton, BaseLabel, BaseInput, BaseTextarea, Loader },
     data() {
         return {
+            messagesLoading: true,
             messages: [],
+            newMessageSaving: false,
             newMessage: {
                 user: '',
                 message: '',
@@ -23,6 +26,9 @@ export default {
 
     methods: {
         sendMessage() {
+            if(this.newMessageSaving) return;
+
+            this.newMessageSaving = true;
             chatSaveMessage({
                 user: this.newMessage.user,
                 message: this.newMessage.message,
@@ -30,6 +36,7 @@ export default {
             })
                 .then(() => {
                     this.newMessage.message = '';
+                    this.newMessageSaving = false;
                 });
         },
 
@@ -39,8 +46,10 @@ export default {
     },
 
     mounted() {
+        this.messagesLoading = true;
         chatSubscribeToMessages(messages => {
             this.messages = messages;
+            this.messagesLoading = false;
         });
     }
 };
@@ -52,15 +61,20 @@ export default {
     <p class="mb-3">Leyendo los mensajes del chat, ahora en tiempo real</p>
 
     <div class="flex justify-between gap-4">
-        <div >
-            <div 
-                v-for="message in messages"
-                :key="message.id"
-                class="mb-2"
-            >
-                <div><b>Usuario:</b> {{ message.user }}</div>
-                <div><b>Mensaje:</b> {{ message.message }}</div>
-                <div class="text-right">{{ formatDate(message.created_at) }}</div>
+        <div>
+            <div v-if="!messagesLoading">
+                <div 
+                    v-for="message in messages"
+                    :key="message.id"
+                    class="mb-2"
+                >
+                    <div><b>Usuario:</b> {{ message.user }}</div>
+                    <div><b>Mensaje:</b> {{ message.message }}</div>
+                    <div class="text-right">{{ formatDate(message.created_at) }}</div>
+                </div>
+            </div>
+            <div v-else>
+                <Loader></Loader>
             </div>
         </div>
 
@@ -84,7 +98,9 @@ export default {
                 ></BaseTextarea>
             </div>
 
-            <BaseButton />
+            <BaseButton 
+                :loading="newMessageSaving"
+            />
 
         </form>
     </div>
